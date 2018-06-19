e5de43d3f5a9eb756e3705f489bf68178e295e7b
scottlepp <scott.lepper@gmail.com>
2018-06-19 15:03:16 -0400

    organize

--- src/app-class.ts
+++ src/app-class.ts
@@ -1,5 +1,5 @@
 import { Component, Vue, Prop, Provide } from 'vue-property-decorator'
-import Header from './header.vue';
+import Header from './header/header.vue';
 
 @Component({
   components: {
@@ -9,7 +9,7 @@ import Header from './header.vue';
 export default class App extends Vue {
 
   @Provide() 
-  foo = 'fooy';
+  foo = 'foo';
 
   @Provide() 
   bar = 'bar';
